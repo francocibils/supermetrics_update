d232c0cdb33aa3e5efd16bac34cdf469e171802d
francocibils <108623588+francocibils@users.noreply.github.com>
2024-04-17 10:08:22 -0300

    Update helper_functions.py
    Added date functionality. This allows to group by date when specified.

--- helper_functions.py
+++ helper_functions.py
@@ -9,29 +9,41 @@ def get_file_type(file):
     elif file.name.endswith(".xls"):
         return 'xls'
     
-def processing_amazon_sellerboard(amz_df, amz_listing_df):
+def processing_amazon_sellerboard(amz_df, amz_listing_df, date = False):
 
     amz_listing_df = amz_listing_df[['ASIN', 'PRODUCT', 'NAME', 'SKU', 'BRAND', 'PRODUCTO']]
     amz_listing_df = amz_listing_df.rename(columns = {'PRODUCTO': 'Type of product', 'BRAND': 'Brand'})
 
-    amz_df = amz_df[['Product', 'ASIN', 'SKU', 'Units', 'Sales', 'Ads']]
+    if date:
+        amz_df = amz_df[['FECHA', 'Product', 'ASIN', 'SKU', 'Units', 'Sales', 'Ads']]
+    else:
+        amz_df = amz_df[['Product', 'ASIN', 'SKU', 'Units', 'Sales', 'Ads']]
+
     amz_df['Ads'] = amz_df['Ads'].abs()
 
     amz_listing_merge = amz_listing_df[['SKU', 'Brand', 'Type of product']]
     amz_df = pd.merge(amz_df, amz_listing_merge, on = 'SKU', how = 'left').drop_duplicates()
 
     na_mask = amz_df['Brand'].isna() | amz_df['Type of product'].isna()
-    amz_na_df = amz_df[na_mask][['Product', 'ASIN', 'SKU', 'Units', 'Sales', 'Ads']]
-    amz_grouped_df = amz_df.groupby(['Brand', 'Type of product'])[['Ads', 'Units', 'Sales']].sum()
+
+    if date:
+        amz_na_df = amz_df[na_mask][['FECHA', 'Product', 'ASIN', 'SKU', 'Units', 'Sales', 'Ads']]
+        amz_grouped_df = amz_df.groupby(['FECHA', 'Brand', 'Type of product'])[['Ads', 'Units', 'Sales']].sum()
+    else:
+        amz_na_df = amz_df[na_mask][['Product', 'ASIN', 'SKU', 'Units', 'Sales', 'Ads']]
+        amz_grouped_df = amz_df.groupby(['Brand', 'Type of product'])[['Ads', 'Units', 'Sales']].sum()
 
     return amz_grouped_df, amz_na_df
 
-def processing_amazon_sellercentral(amz_df, amz_listing_df):
+def processing_amazon_sellercentral(amz_df, amz_listing_df, date = False):
 
     amz_listing_df = amz_listing_df[['ASIN', 'PRODUCT', 'NAME', 'SKU', 'BRAND', 'PRODUCTO']]
     amz_listing_df = amz_listing_df.rename(columns = {'PRODUCTO': 'Type of product', 'BRAND': 'Brand'})
 
-    amz_df = amz_df[['ASIN (parent)', 'ASIN (child)', 'Título', 'SKU', 'Sesiones: total', 'Vistas de página: total']]
+    if date:
+        amz_df = amz_df[['FECHA', 'ASIN (parent)', 'ASIN (child)', 'Título', 'SKU', 'Sesiones: total', 'Vistas de página: total']]
+    else:
+        amz_df = amz_df[['ASIN (parent)', 'ASIN (child)', 'Título', 'SKU', 'Sesiones: total', 'Vistas de página: total']]
 
     amz_df['Sesiones: total'] = amz_df['Sesiones: total'].str.replace(',', '').astype(int)
     amz_df['Vistas de página: total'] = amz_df['Vistas de página: total'].str.replace(',', '').astype(int)
@@ -40,7 +52,12 @@ def processing_amazon_sellercentral(amz_df, amz_listing_df):
     amz_df = pd.merge(amz_df, amz_listing_merge, on = 'SKU', how = 'left').drop_duplicates()
 
     na_mask = amz_df['Brand'].isna() | amz_df['Type of product'].isna()
-    amz_na_df = amz_df[na_mask][['ASIN (parent)', 'ASIN (child)', 'Título', 'SKU', 'Sesiones: total', 'Vistas de página: total']]
-    amz_grouped_df = amz_df.groupby(['Brand', 'Type of product'])[['Sesiones: total', 'Vistas de página: total']].sum()
+
+    if date:
+        amz_na_df = amz_df[na_mask][['FECHA', 'ASIN (parent)', 'ASIN (child)', 'Título', 'SKU', 'Sesiones: total', 'Vistas de página: total']]
+        amz_grouped_df = amz_df.groupby(['FECHA', 'Brand', 'Type of product'])[['Sesiones: total', 'Vistas de página: total']].sum()
+    else:
+        amz_na_df = amz_df[na_mask][['ASIN (parent)', 'ASIN (child)', 'Título', 'SKU', 'Sesiones: total', 'Vistas de página: total']]
+        amz_grouped_df = amz_df.groupby(['Brand', 'Type of product'])[['Sesiones: total', 'Vistas de página: total']].sum()
 
     return amz_grouped_df, amz_na_df
